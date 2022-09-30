c67665e567da02f22106dfe6f4f4ba064b237906
tommygaessler <tommy.gaessler@zoom.us>
2022-09-30 00:16:00 -0600

    updating to 2.8.0

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './App.css';
 import { ZoomMtg } from '@zoomus/websdk';
 
-ZoomMtg.setZoomJSLib('https://source.zoom.us/2.7.0/lib', '/av');
+ZoomMtg.setZoomJSLib('https://source.zoom.us/2.8.0/lib', '/av');
 
 ZoomMtg.preLoadWasm();
 ZoomMtg.prepareWebSDK();
