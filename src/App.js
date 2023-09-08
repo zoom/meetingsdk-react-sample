682a9981f9595d281f9b8e5b482b6dc5a50bd735
tommygaessler <tommy.gaessler@zoom.us>
2023-09-08 14:57:32 -0600

    updated to version 2.16.0

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './App.css';
 import { ZoomMtg } from '@zoomus/websdk';
 
-ZoomMtg.setZoomJSLib('https://source.zoom.us/2.15.2/lib', '/av');
+ZoomMtg.setZoomJSLib('https://source.zoom.us/2.16.0/lib', '/av');
 
 ZoomMtg.preLoadWasm();
 ZoomMtg.prepareWebSDK();
