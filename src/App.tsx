5a2e9a10c5c42e1bdbb71c171474f3c70d9241b0
Ben Bobby Stef <benjaminstef@gmail.com>
2024-10-25 14:27:56 -0400

    Update App.tsx
    moved the leave URL

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
   const userEmail = "";
   const registrantToken = "";
   const zakToken = "";
-  const leaveUrl = "https://bstef.github.io/thankyou.html";
+  const leaveUrl = "https://bstef.github.io/thanks.html";
 
   const getSignature = async () => {
     try {
