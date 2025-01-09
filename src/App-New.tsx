9f84b64e535b410ec761e90ab95fd3f96c2102fa
Nithiyanantham <r.nandhanraghu123@gmail.com>
2025-01-09 16:48:36 +0530

    Adding API request for ZAK token

--- src/App-New.tsx
+++ src/App-New.tsx
@@ -1,36 +1,120 @@
 import "./App.css";
 import ZoomMtgEmbedded from "@zoom/meetingsdk/embedded";
+import jsrsasign from 'jsrsasign';
+import {useEffect, useState} from "react";
 
 function App() {
   const client = ZoomMtgEmbedded.createClient();
 
-  const authEndpoint = ""; // http://localhost:4000
-  const sdkKey = "";
-  const meetingNumber = "";
-  const passWord = "";
-  const role = 0;
-  const userName = "React";
-  const userEmail = "";
+  const authEndpoint = "http://localhost:4000"; // http://localhost:4000
+  const sdkKey = import.meta.env.VITE_APP_CLIENT_ID_SDK;
+  const secret = import.meta.env.VITE_APP_CLIENT_SECRET_SDK;
+  const meetingNumber = import.meta.env.VITE_APP_MN;
+  const passWord = import.meta.env.VITE_APP_PASSCODE;
+  // const zakToken = import.meta.env.VITE_APP_ZAKTOKEN;
+  const role = 1;
+  const userName = "Dhilip";
+  const userEmail = "dhilipkumar2005@gmail.com";
   const registrantToken = "";
-  const zakToken = "";
+  const [zakToken,setZakTok] = useState("");
 
-  const getSignature = async () => {
+  // const getSignature = async () => {
+  //   try {
+  //     const req = await fetch(authEndpoint, {
+  //       method: "POST",
+  //       headers: { "Content-Type": "application/json" },
+  //       body: JSON.stringify({
+  //         meetingNumber: meetingNumber,
+  //         role: role,
+  //       }),
+  //     });
+  //     const res = await req.json()
+  //     const signature = res.signature as string;
+  //     startMeeting(signature)
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // };
+
+  async function getAccessToken(){
     try {
-      const req = await fetch(authEndpoint, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          meetingNumber: meetingNumber,
-          role: role,
-        }),
+      const response = await fetch(`${authEndpoint}/get-access-token`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
       });
-      const res = await req.json()
-      const signature = res.signature as string;
-      startMeeting(signature)
+  
+      if (!response.ok) {
+        console.error("Failed to fetch access token");
+        return;
+      }
+  
+      const data = await response.json();
+      console.log("Access Token:", data.access_token);
+      ZakToken(data.access_token);
+    } catch (e) {
+      console.error("Error fetching access token:", e);
+    }
+  }
+   
+  async function ZakToken(access_token: string){
+    try {
+      const response = await fetch(`${authEndpoint}/zakToken`, {
+        method: "POST",
+        body:JSON.stringify({
+          "accesstoken":`${access_token}`
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      });
+      if (!response.ok) {
+        console.error("Failed to fetch access token");
+        return;
+      }
+  
+      const data = await response.json();
+      console.log(data.token);
+      setZakTok(data.token);
     } catch (e) {
       console.log(e);
     }
-  };
+  }
+
+  function getSignature(sdkKey: string,secret: string,meetingNumber: number,role: number){
+      const signature = generateSignature(sdkKey,secret,meetingNumber,role);
+      if(signature){
+        startMeeting(signature);
+      }else{
+        console.log("signature not Exist");
+      }
+    }
+  
+    function generateSignature(key: string, secret: string, meetingNumber: number, role: number) {
+  
+      const iat = Math.round(new Date().getTime() / 1000) - 30
+      // const iat = new Date().getTime() - 30000
+      console.log(iat)
+      const exp = iat + 60 * 60 * 2
+      const oHeader = { alg: 'HS256', typ: 'JWT' }
+    
+      const oPayload = {
+        sdkKey: key,
+        appKey: key,
+        mn: meetingNumber,
+        role: role,
+        iat: iat,
+        exp: exp,
+        tokenExp: exp
+      }
+    
+      const sHeader = JSON.stringify(oHeader)
+      const sPayload = JSON.stringify(oPayload)
+      const sdkJWT = jsrsasign.KJUR.jws.JWS.sign('HS256', sHeader, sPayload, secret)
+      console.log(sdkJWT);
+      return sdkJWT
+    }
 
   async function startMeeting(signature: string) {
     const meetingSDKElement = document.getElementById("meetingSDKElement")!;
@@ -57,6 +141,10 @@ function App() {
     }
   }
 
+  useEffect(()=>{
+    getAccessToken()
+  },[])
+
   return (
     <div className="App">
       <main>
@@ -65,7 +153,7 @@ function App() {
         <div id="meetingSDKElement">
           {/* Zoom Meeting SDK Component View Rendered Here */}
         </div>
-        <button onClick={getSignature}>Join Meeting</button>
+        <button onClick={()=>getSignature(sdkKey,secret,meetingNumber,role)}>Join Meeting</button>
       </main>
     </div>
   );
--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,78 @@
 import "./App.css";
 import { ZoomMtg } from "@zoom/meetingsdk";
+import jsrsasign from 'jsrsasign';
+import { useEffect, useState } from "react";
+
 
 ZoomMtg.preLoadWasm();
 ZoomMtg.prepareWebSDK();
 
 function App() {
-  const authEndpoint = ""; // http://localhost:4000
-  const sdkKey = "";
-  const meetingNumber = "";
-  const passWord = "";
-  const role = 0;
-  const userName = "React";
-  const userEmail = "";
+  const authEndpoint = "http://localhost:4000"; // http://localhost:4000
+  const sdkKey = import.meta.env.VITE_APP_CLIENT_ID_SDK;
+  const secret = import.meta.env.VITE_APP_CLIENT_SECRET_SDK;
+  const meetingNumber = import.meta.env.VITE_APP_MN;
+  const passWord = import.meta.env.VITE_APP_PASSCODE;
+  // const zakToken = import.meta.env.VITE_APP_ZAKTOKEN;
+  const leaveUrl = import.meta.env.VITE_APP_LEAVEURL;
+  const role = 0; //0 represents participant and 1 represents host
+  const userName = "Dhilip";
+  const userEmail = "dhilipkumar2005@gmail.com";
   const registrantToken = "";
-  const zakToken = "";
-  const leaveUrl = "http://localhost:5173";
+  const [zakToken,setZakTok] = useState("");
+  
+ 
+   // need to generate based on meeting id - using - role by default 0 = javascript
+  
 
-  const getSignature = async () => {
+async function getAccessToken(){
+  try {
+    const response = await fetch(`${authEndpoint}/get-access-token`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      console.error("Failed to fetch access token");
+      return;
+    }
+
+    const data = await response.json();
+    console.log("Access Token:", data.access_token);
+    ZakToken(data.access_token);
+  } catch (e) {
+    console.error("Error fetching access token:", e);
+  }
+}
+ 
+async function ZakToken(access_token: string){
+  try {
+    const response = await fetch(`${authEndpoint}/zakToken`, {
+      method: "POST",
+      body:JSON.stringify({
+        "accesstoken":`${access_token}`
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    });
+    if (!response.ok) {
+      console.error("Failed to fetch access token");
+      return;
+    }
+
+    const data = await response.json();
+    console.log(data.token);
+    setZakTok(data.token);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+
+  const getSignature_request = async () => {
     try {
       const req = await fetch(authEndpoint, {
         method: "POST",
@@ -33,8 +89,41 @@ function App() {
       console.log(e);
     }
   };
+  function getSignature(sdkKey: string,secret: string,meetingNumber: number,role: number){
+    const signature = generateSignature(sdkKey,secret,meetingNumber,role);
+    if(signature){
+      startMeeting(signature);
+    }else{
+      console.log("signature doesn't exit");
+    }
+  }
 
-  function startMeeting(signature: string) {
+  function generateSignature(key: string, secret: string, meetingNumber: number, role: number) {
+
+    const iat = Math.round(new Date().getTime() / 1000) - 30
+    // const iat = new Date().getTime() - 30000
+    console.log(iat)
+    const exp = iat + 60 * 60 * 2
+    const oHeader = { alg: 'HS256', typ: 'JWT' }
+  
+    const oPayload = {
+      sdkKey: key,
+      appKey: key,
+      mn: meetingNumber,
+      role: role,
+      iat: iat,
+      exp: exp,
+      tokenExp: exp
+    }
+  
+    const sHeader = JSON.stringify(oHeader)
+    const sPayload = JSON.stringify(oPayload)
+    const sdkJWT = jsrsasign.KJUR.jws.JWS.sign('HS256', sHeader, sPayload, secret)
+    console.log(sdkJWT);
+    return sdkJWT
+  }
+
+  function startMeeting(signature: any) {
     document.getElementById("zmmtg-root")!.style.display = "block";
 
     ZoomMtg.init({
@@ -67,11 +156,16 @@ function App() {
     });
   }
 
+
+  useEffect(()=>{
+    getAccessToken();
+  },[])
   return (
     <div className="App">
       <main>
         <h1>Zoom Meeting SDK Sample React</h1>
-        <button onClick={getSignature}>Join Meeting</button>
+        <button onClick={()=>getSignature(sdkKey,secret,meetingNumber,role)}>Join Meeting</button>
+        <button onClick={getSignature_request}>Join Meeting (Backend Request)</button>
       </main>
     </div>
   );
